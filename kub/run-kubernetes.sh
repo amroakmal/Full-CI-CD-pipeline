4d01dc36dcc49b188321ef0515007f08c825a409
AmrAkmal <amrakmal97@gmail.com>
2020-08-25 01:20:24 +0200

    edits

--- kub/run-kubernetes.sh
+++ kub/run-kubernetes.sh
@@ -1,4 +1,4 @@
-kubectl create -f deployment.yml
-kubectl create -f service.yml
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
 
 kubectl set image deployment proj-deploy nginx=nginx:1.9
